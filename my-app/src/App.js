794db0ee7821008b8c1e8f0c5efab869da4a64f8
demqqo <138254074+demqqo@users.noreply.github.com>
2024-03-27 20:51:45 -0400

    Create App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React  from 'react';
+import './App.css';
+import Searchbox from "./searchbox.js"
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1>
+     <Searchbox />
+    </div>
+  );
+}
+
+export default App;
